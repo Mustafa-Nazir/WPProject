9ef9f451887f9a8ac910326a7ef34937e07f8168
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2022-12-24 21:30:55 +0300

    added models

---
+++ DataAccess/Concrete/EntityFramework/WPProjectDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class WPProjectDbContext:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=WPProjectDb;Trusted_Connection=true");
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Emoji> Emojis { get; set; }
+        public DbSet<Follower> Followeres { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImageDetail> ImageDetails { get; set; }
+    }
+}
---
+++ Entities/Concrete/Image.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Image : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? Path { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
