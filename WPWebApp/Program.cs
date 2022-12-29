ec148ecb91926c2a2b85a9e712a893cfae544284
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2022-12-29 11:40:40 +0300

    added IoC

--- WPWebApp/Program.cs
+++ WPWebApp/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WPWebApp.Data;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Business.DependencyResolvers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,10 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDependencyResolvers(new IServiceModule[] {
+                new BusinessModule()
+            });
 
 var app = builder.Build();
 
